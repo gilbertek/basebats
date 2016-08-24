45a7388c5b2b35d26ed662b6c0cc12252cced0c7
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2016-08-23 22:40:05 -0400

    updates

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,24 @@
 module Main where
 
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Foldable as F
+
+-- from cassava
+import Data.Csv.Streaming
+
+-- a simple type alias for data
+type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
+fourth :: (a, b, c, d) -> d
+fourth (_, _, _, d) = d
+
+baseballStats :: BL.ByteString -> Records BaseballStats
+baseballStats = decode NoHeader
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  csvData <- BL.readFile "batting.csv"
+  let summed  = F.foldr summer 0 (baseballStats csvData)
+
+  putStrLn $ "Total atBats was: " ++ (show summed)
+  where summer = (+) . fourth
+
